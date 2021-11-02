b549ad89c417694312f86cb87d4ab29c4d69da90
MunavvarSinan <munavvarsinan8@gmail.com>
2021-11-03 02:59:40 +0530

    heroku

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import 'dotenv/config.js';
 import http from 'http';
 import server from './server/index.js';
+import 'babel-polyfill'
 
+import 'babel-core/register'
 const PORT = process.env.PORT || 5000;
 http.createServer({}, server).listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
