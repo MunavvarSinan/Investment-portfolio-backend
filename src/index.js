956a979778a5c8288eab2982674591a958dda1f8
MunavvarSinan <munavvarsinan8@gmail.com>
2021-11-03 02:10:02 +0530

    heroku error

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import 'dotenv/config.js';
 import http from 'http';
 import server from './server/index.js';
 import 'babel-polyfill'
+
 import 'babel-core/register'
 const PORT = process.env.PORT || 5000;
 http.createServer({}, server).listen(PORT, () => {
